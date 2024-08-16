e39d122d8e0795a4f4139587b54d55d100ca1518
Harry <ioioooio@163.com>
2024-08-16 23:36:40 +0800

    debug for the price generator

--- src/main/java/com/portfolio/monitor/generator/GeometricBrownianMotionPriceGenerator.java
+++ src/main/java/com/portfolio/monitor/generator/GeometricBrownianMotionPriceGenerator.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.Random;
 
 @Component
@@ -12,26 +13,39 @@ public class GeometricBrownianMotionPriceGenerator {
     private static final Random random = new Random();
 
     public static void main(String[] args) throws InterruptedException {
+        GeometricBrownianMotionPriceGenerator generator = new GeometricBrownianMotionPriceGenerator();
         // Parameters
-        double mu = 0.05; // Expected return
-        double sigma = 0.2; // Annualized standard deviation
-        double deltaT = 1.0 / 7257600; // Time increment in years (1 second)
+        double mu = 0.15; // Expected return
+        double sigma = 0.3; // Annualized standard deviation
+        double deltaT = 1.0 / 7257600; // Time increment in years (1 week)
 
         // Generate random relative change
         double stockPrice = 190.0;
-        for (int i = 1; i <= 10; i++) {
-            double relativeChange = generateRelativeChange(mu, sigma, deltaT);
-            stockPrice += relativeChange;
+        DecimalFormat df = new DecimalFormat("#.00");
+        while (true) {
+            double relativeChange = generator.generateRelativeChange(mu, sigma, deltaT);
+            stockPrice *= (1 + relativeChange);
             if (stockPrice < 0) {
                 stockPrice = 0;
             }
             System.out.println("Relative Change: " + relativeChange);
-            System.out.println("Stock price: " + stockPrice);
+            System.out.println("Stock price: " + df.format(stockPrice));
             Thread.sleep(1000);
         }
     }
 
-    public static double generateRelativeChange(double mu, double sigma, double deltaT) {
+    /*public double generateStockPrice(double mu, double sigma, double deltaT) {
+        DecimalFormat df = new DecimalFormat("#.00");
+        double stockPrice = 190.0;
+        double relativeChange = generateRelativeChange(mu, sigma, deltaT);
+        stockPrice *= (1 + relativeChange);
+        if (stockPrice < 0) {
+            stockPrice = 0;
+        }
+        return df.format(stockPrice);
+    }*/
+
+    private double generateRelativeChange(double mu, double sigma, double deltaT) {
         // Standard normal distribution
         double epsilon = random.nextGaussian();
 
